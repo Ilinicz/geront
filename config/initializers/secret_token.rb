0802de0f703d3f32bcdadd68fb7fa32060e3e40d
Vincent Ilinicz <vilinicz@mail.ru>
2013-12-24 16:41:31 +0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Geront::Application.config.secret_key_base = '3a3d66ec70f0ffb85c824959b0bfcb1892c85f811aea48156b5bd74021bf444290f386818465ac9785dec40b6d7cc9088b153512be33d578852cd3368469b7c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Geront::Application.config.session_store :cookie_store, key: '_geront_session'
